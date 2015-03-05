5dcf22885a804f727735626d08e9e1ca10aa7d18
Arsen <1b2c@mail.ru>
2015-03-05 22:15:40 +0400

    global fix

--- config/web.php
+++ config/web.php
@@ -9,7 +9,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => 'J9jJ_TsmEcSUrWhcaosv-oaQpHZ68nCI',
+            'cookieValidationKey' => 'tVfAX3rkC5EigPE4BdxUpC7kGiOaJcjY',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
